386d815625fe25e1f2ecfda0baf7e7dbf09e3e8e
Le Thanh Quoc <quoclt@Les-MacBook-Pro.local>
2020-02-29 13:22:20 +0700

    add example clean code

---
+++ oop/guess_statistics_message.php
@@ -0,0 +1,51 @@
+<?php
+class GuessStasticsMessage
+{
+    private $number;
+    private $verb;
+    private $pluralModifier;
+
+    public function makeMessage($candidate, $count)
+    {
+        $this->createPluralDependentMessageParts($count);
+        return sprintf("There %s %s %s%s", $this->verb, $this->number, $candidate, $this->pluralModifier);
+    }
+
+    private function createPluralDependentMessageParts($count)
+    {
+        if ($count == 0) {
+            $this->thereAreNoLetters();
+        } elseif ($count == 1) {
+            $this->thereIsOneLetter();
+        } else {
+            $this->thereAreManyLetters($count);
+        }
+    }
+
+    private function thereAreManyLetters($count)
+    {
+        $this->number = $count;
+        $this->verb = "are";
+        $this->pluralModifier = "s";
+    }
+
+    private function thereIsOneLetter()
+    {
+        $this->number = 1;
+        $this->verb = "is";
+        $this->pluralModifier = "";
+    }
+
+    private function thereAreNoLetters()
+    {
+        $this->number = "no";
+        $this->verb = "are";
+        $this->pluralModifier = "s";
+    }
+}
+
+$message = new GuessStasticsMessage();
+echo $message->makeMessage('dog', 4);
+echo "\n";
+
+
