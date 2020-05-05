8c4a0cc4b8d953fd91d299ee9bf503cbbf28adb3
Le Thanh Quoc <lethanhquoc@AIO0103.local>
2020-05-05 17:51:47 +0700

    20200505

--- Manual/PDO/Transaction.php
+++ Manual/PDO/Transaction.php
@@ -5,14 +5,42 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->beginTransaction();
     $pdo->exec(
-        "INSERT INTO `orders` (`orderNumber`, `orderDate`, `requiredDate`, `shippedDate`, `status`, `comments`, `customerNumber`)VALUES (10426, '2020-05-04', '2020-05-10', NULL, 'In Process', NULL, 119);"
+        "INSERT INTO `orders` (
+            `orderNumber`,
+            `orderDate`,
+            `requiredDate`,
+            `shippedDate`,
+            `status`,
+            `comments`,
+            `customerNumber`
+        ) VALUES (
+            10427,
+            '2020-05-04',
+            '2020-05-10',
+            null,
+            'In Process',
+            null,
+            119
+        );"
     );
     $pdo->exec(
-        "INSERT INTO `orderdetails` (`orderNumber`, `productCode`, `quantityOrdered`, `priceEach`, `orderLineNumber`) VALUES (10426, 'S24_2022', 24, 36.74, 4);"
+        "INSERT INTO `orderdetails` (
+            `orderNumber`,
+            `productCode`,
+            `quantityOrdered`,
+            `priceEach`,
+            `orderLineNumber`
+        ) VALUES (
+            10427,
+            'S24_2022',
+            24,
+            36.74,
+            4
+        );"
     );
     $pdo->commit();
 } catch (Exception $e) {
     $pdo->rollBack();
-    echo "Failed: " . $e->getMessage();
+    echo "Failed: " . $e->getMessage() . PHP_EOL;
 }
 
